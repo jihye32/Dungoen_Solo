deb72cedaab3cd3bb81b67fb368d652ed2371bf6
jihye <thoth_0302@naver.com>
2024-02-06 12:36:28 +0900

    Feature - NPC 설명란

--- Assets/Scripts/NPC/StoreNPC.cs
+++ Assets/Scripts/NPC/StoreNPC.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class StoreNPC : MonoBehaviour
 {
     private GameObject player;
+    private GameObject storeUseUI;
+    private NPCUI npuUI;
     
 
-    private void Awake()
+    private void Start()
     {
         player = GameManager.instance.player;
+        npuUI = GetComponentInParent<NPCUI>();
+        storeUseUI = npuUI.useStoreUI;
+    }
+
+    private void Update()
+    {
+        OnUseStore();
     }
 
     private void OnUseStore()
     {
         if(transform.position.x - player.transform.position.x < 1 && transform.position.x - player.transform.position.x > -1)
         {
-
+            storeUseUI.SetActive(true);
         }
+        else storeUseUI.SetActive(false);
     }
 }
--- Assets/Scripts/UI/NPCUI.cs
+++ Assets/Scripts/UI/NPCUI.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class NPCUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject useStoreUI;
 }
