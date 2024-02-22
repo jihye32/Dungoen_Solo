41c5bc340eeb6795f6f1c383da261b2db23de117
jihye <thoth_0302@naver.com>
2024-02-22 17:39:02 +0900

    <fix> monobehaviour 없으니깐 unity랑 연결 안댐

--- Assets/Scripts/Object/Item.cs
+++ Assets/Scripts/Object/Item.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item
+public class Item : MonoBehaviour
 {
     public ItemData itemData;
     [HideInInspector] public int attack;
