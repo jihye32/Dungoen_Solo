aed79123dddf5c80fb24be08a551ebbc823427dc
jihye <thoth_0302@naver.com>
2024-02-22 16:49:52 +0900

    <fix> MonoBehaviour 삭제

--- Assets/Scripts/Object/Item.cs
+++ Assets/Scripts/Object/Item.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour
+public class Item
 {
     public ItemData itemData;
     [HideInInspector] public int attack;
